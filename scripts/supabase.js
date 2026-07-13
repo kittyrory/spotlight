d411d2b457d3d1e7f76be28d7451be9085896505
rory 3 <253945638+kittyrory@users.noreply.github.com>
2026-07-13 15:33:51 -0400

    Refactor Supabase client initialization

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,7 +1,9 @@
 // connects supabase for auth and user data
 // railway will also be used for separate purposes
 
-  const supabase = window.supabase.createClient(
-    'https://qhcohcxmxndexkhexbst.supabase.co',
-    'sb_publishable_mX0YOWlolguEx3j0uCjtmQ_VlMdEYhz'
-  );
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://qhcohcxmxndexkhexbst.supabase.co',
+const supabaseAnonKey = 'sb_publishable_mX0YOWlolguEx3j0uCjtmQ_VlMdEYhz'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
